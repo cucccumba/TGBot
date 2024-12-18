82544109b25abdb6748bf201fd058b37da724042
Ilia Artemev <55094588+cucccumba@users.noreply.github.com>
2024-12-18 19:52:02 +0100

    Add files via upload

---
+++ src/main/java/TGBot/TGBot.java
@@ -0,0 +1,42 @@
+package TGBot;
+
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TGBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotUsername() {
+        return "ILUXA_BOT_121_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7668754886:AAF6Jw4dDL4z30zo0T6lOc9I4fc2j-onI2E";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+}
